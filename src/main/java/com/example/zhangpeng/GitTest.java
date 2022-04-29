2daf1e0c65fc11aa93e386047373851e9cfa40bd
zhangpeng <1547143416@qq.com>
2022-04-29 22:40:00 +0800

    first commit

---
+++ src/main/java/com/example/zhangpeng/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.zhangpeng;
+
+/**
+ * @author 章鹏
+ * @create 2022-04-29  22:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hell0.git");
+    }
+}
---
+++ src/test/java/com/example/zhangpeng/gittest/GitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhangpeng.gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
