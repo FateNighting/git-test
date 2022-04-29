0a712ed9a532e76cc6fe5212a5dc437045daf740
FateNight <37129757+FateNighting@users.noreply.github.com>
2022-04-29 23:45:21 +0800

    pull commit

--- src/main/java/com/example/zhangpeng/GitTest.java
+++ src/main/java/com/example/zhangpeng/GitTest.java
@@ -13,6 +13,6 @@ public class GitTest {
         System.out.println("hell0.git3");
         System.out.println("master test");
         System.out.println("push test");
-
+        System.out.println("pull test");
     }
 }
